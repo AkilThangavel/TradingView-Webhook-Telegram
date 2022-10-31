21aee01dafe2e01e006b76b34b2fe5eedd8a0bff
Akil1996 <akilh4@gmail.com>
2022-10-31 17:11:00 +0530

    First commit

---
+++ config.py
@@ -0,0 +1,35 @@
+#API Keys
+sec_key = (
+    "abc@123"  # Can be anything. Has to match with "key" in your TradingView alert message
+)
+
+# Telegram Settings
+send_telegram_alerts = True
+tg_token = "5607909007:AAHPuiVPZE6rZ9Zfx5pR_AZf9mOCRvZDIUE"  # Bot token. Get it from @Botfather
+channel = "-1001849979287"  # Channel ID (ex. -1001487568087)
+
+# Discord Settings
+send_discord_alerts = True                                                                                                                          
+discord_webhook = "1036187440286797834/eNhoqHCKFhcen6BAsDBGHGnQJbInv-gweAqFvVJxhAefMOyEGz765JQVWjSn_UTzeOgP"  # Discord Webhook URL (https://support.discordapp.com/hc/de/articles/228383668-Webhooks-verwenden)
+
+# Slack Settings
+send_slack_alerts = True
+slack_webhook = "T0492D75J0Z/B048PNNR4BD/aaBs7Xx37S3g5TLgAd8u2Oqz"  # Slack Webhook URL (https://api.slack.com/messaging/webhooks)
+
+# Twitter Settings
+send_twitter_alerts = True
+tw_ckey = "HkGJqLEEH2iTBhbYTU8dh3mUr"
+tw_csecret = "y0BmWGeCPRZLY0YaTJgTEPQ4umTv1nbnOIi7GoSKzX3uQj9B4q"
+tw_atoken = "958427873525735424-CeAz6xPIaSGgO7mB6NsiSaJKKw96KdS"
+tw_asecret = "NHMnOTIfgSA4LdidGnHml6iFyP9PzDfHQEaVMcF7aVbON"    
+
+# Email Settings
+send_email_alerts = True
+email_sender = "akiltalgo@gmail.com"  # Your email address
+email_receivers = ["akilh4@gmail.com", "thetwobluepencil@gmail.com"]  # Receivers, can be multiple
+email_subject = "Trade Alert!"
+
+email_port = 465  # SMTP SSL Port (ex. 465)
+email_host = "smtp.gmail.com"  # SMTP host (ex. smtp.gmail.com)
+email_user = "akiltalgo@gmail.com"  # SMTP Login credentials
+email_password = "txlqvaiqpwstgvet"  # SMTP Login credentials
